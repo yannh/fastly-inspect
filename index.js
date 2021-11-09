f73fe05750678fd31b010b2551782f2dee4b9d28
Yann Hamon <yann.hamon@contentful.com>
2021-11-09 23:57:49 +0100

    Use vuejs for rendering

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ import wasmInit, {
   run
 } from "./pkg/fastly_inspect.js";
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    clientip: '',
+    client_asname: ''
+  }
+})
+
 const runWasm = async () => {
   const helloWorld = await wasmInit("./pkg/fastly_inspect_bg.wasm");
   const dns_infos = await run();
   console.log(dns_infos);
-  document.getElementById("client_ip").textContent = dns_infos.client_ip_info.ip;
-  document.getElementById("client_asname").textContent = dns_infos.client_ip_info.as_name;
-  document.getElementById("resolver_ip").textContent = dns_infos.dns_resolver_info.ip;
-  document.getElementById("resolver_asname").textContent = dns_infos.dns_resolver_info.as_name;
+  app.clientip = dns_infos.client_ip_info.ip;
+  app.client_asname = dns_infos.client_ip_info.as_name;
+  app.resolver_ip = dns_infos.dns_resolver_info.ip;
+  app.resolver_asname = dns_infos.dns_resolver_info.as_name;
 };
 
 runWasm();
